0a4104e3ebcbd6280e56111bb4eb23f89d969db6
Alicia <alicia.rsouza@gmail.com>
2023-01-17 22:53:32 -0300

    feat: solução ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -39,9 +39,19 @@ public class Reservation {
         //Converte o valor diff em miliseconds para dias.
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)){
+            return "Reservation dates for update must be future dates.";
+        }
+
+        if (checkIn.after(now) || checkOut.before(now)){
+            return "Check-out date must be after check-in date.";
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
